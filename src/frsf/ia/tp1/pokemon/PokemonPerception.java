fdfd1396143ba643cd73fa100a0e5697b67f9284
Valentin Reynoso <valenreynoso17@gmail.com>
2023-04-25 13:53:30 -0300

    Implementadas las percepciones pokemon

--- src/frsf/ia/tp1/pokemon/PokemonPerception.java
+++ src/frsf/ia/tp1/pokemon/PokemonPerception.java
@@ -6,12 +6,14 @@ import frsf.cidisi.faia.agent.Agent;
 import frsf.cidisi.faia.agent.Perception;
 import frsf.cidisi.faia.environment.Environment;
 import frsf.ia.tp1.pokemon.enemigos.Enemigo;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class PokemonPerception extends Perception {
-	
-    public static int NODO_VACIO_PERCEPTION = 0;
-    public static int POKEBOLA_PERCEPTION = 2;
-    public static int BOSS_FINAL_PERCEPTION = 3;
     
     // Que hay en el nodo actual
     private int contenidoNodoActual;
@@ -28,15 +30,49 @@ public class PokemonPerception extends Perception {
 		PokemonEnvironment pokemonEnviroment = (PokemonEnvironment) environment;
 		PokemonEnvironmentState environmentState = pokemonEnviroment.getEnvironmentState();
 		
-		// Devuelve 0 si es vacio, 1 si es enemigo, 2 si es punto de energia y 3 si es el boss final
+		// Contiene 0 si es vacio, 2 si es punto de energia y 3 si es el boss final
 		contenidoNodoActual = environmentState.getMapaAmbiente().get(environmentState.getPosicionAgente());
+		energiaEnemigoEnNodoActual = 0;
 		
-		for (Enemigo e : environmentState.getListaEnemigos()) {
-			
+		mapaPorSatelite = new HashMap<>();
+		
+		/* 
+		 * 	Por cada sucesor, se lo carga en el mapa contenidoNodosSucesores y se carga su respectivo
+		 *	estado que figura en el mapa 
+		 */
+		for (Integer nodo : environmentState.getMapaSucesoresAmbiente().get(environmentState.getPosicionAgente())) {
+			contenidoNodosSucesores.put(nodo, environmentState.getMapaAmbiente().get(nodo));
 		}
 		
-		energiaEnemigoEnNodoActual = environmentState.getListaEnemigos().get(environmentState.getPosicionAgente()).getEnergia();
+		// Si se tiene el satelite, se setea la bandera para que lo sepa el agente
+		if (environmentState.getTurnosRestantesParaUtilizarSatelite() == 0) {
+			puedeUsarSatelite = true;
+			mapaPorSatelite = (HashMap<Integer, Integer>) environmentState.getMapaSucesoresAmbiente().clone();
+		}
 		
+		/*
+		 * Por cada enemigo:
+		 */
+		for (Enemigo e : environmentState.getListaEnemigos()) {
+			// Si hay un enemigo en el nodo del agente, se le pasa la energia del mismo
+			if (e.getNodo() == environmentState.getPosicionAgente()) {
+				contenidoNodoActual = 1;
+				energiaEnemigoEnNodoActual = e.getEnergia();
+			}
+			
+			// Si hay un enemigo en un sucesor, se agrega esta informacion a contenidoNodosSucesores
+			if (contenidoNodosSucesores.keySet().contains(e.getNodo()))	{
+				contenidoNodosSucesores.put(e.getNodo(), 1);
+			}
+			
+			// Si se tiene que pasar el satelite, se agrega la informacion de que hay un enemigo al mapa
+			if (puedeUsarSatelite) mapaPorSatelite.put(e.getNodo(), 1);
+		}
+		
+		// Si el agente esta en el nodo del jefe final, se le pasa la energia del jefe
+		if (environmentState.getPosicionAgente() == environmentState.getJefeFinal().getNodo()) {
+			energiaEnemigoEnNodoActual = environmentState.getJefeFinal().getEnergia();
+		}
 	}
 
 }
