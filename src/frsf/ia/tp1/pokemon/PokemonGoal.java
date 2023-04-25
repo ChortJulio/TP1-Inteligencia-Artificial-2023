bdb85d9410263a269cf92131dad25e84ad69d4dc
Valentin Reynoso <valenreynoso17@gmail.com>
2023-04-25 13:52:33 -0300

    Eliminado codigo comentado de PokemonGoal

--- src/frsf/ia/tp1/pokemon/PokemonGoal.java
+++ src/frsf/ia/tp1/pokemon/PokemonGoal.java
@@ -3,14 +3,12 @@ package frsf.ia.tp1.pokemon;
 import frsf.cidisi.faia.agent.search.GoalTest;
 import frsf.cidisi.faia.state.AgentState;
 
-public class PokemonGoal extends GoalTest{
+public class PokemonGoal extends GoalTest {
 
     @Override
     public boolean isGoalState(AgentState agentState) {
-        //if (((PacmanAgentState) agentState).isNoMoreFood() &&
-        //        ((PacmanAgentState) agentState).isAllWorldKnown()) {
-        //    return true;
-        //}
+        PokemonAgentState estadoPokemon = (PokemonAgentState) agentState;
+        
         return false;
     }
 }
