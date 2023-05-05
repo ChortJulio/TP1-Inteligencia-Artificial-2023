dd9fb0baa1d05403c1ef473a36f80ee10e0a010f
ChortJulio <chortjulio@gmail.com>
2023-05-05 09:51:25 -0300

    Agregados estados iniciales y sucesores de los nodos a la clase Const

--- src/frsf/ia/tp1/pokemon/Const.java
+++ src/frsf/ia/tp1/pokemon/Const.java
@@ -1,5 +1,8 @@
 package frsf.ia.tp1.pokemon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Const {
 
 	public static long seed = 1;
@@ -14,4 +17,65 @@ public class Const {
 	public static int turnosMinimoHastaMoverseEnemigo = 1;
 	public static int turnosParaUtilizarSatelite = 10;
 	public static int turnosRestantesParaReabastecerPokebolas = 3;
+	
+	public static int contenidoInicialNodo1 = 0;
+	public static int contenidoInicialNodo2 = 0;
+	public static int contenidoInicialNodo3 = 0;
+	public static int contenidoInicialNodo4 = 0;
+	public static int contenidoInicialNodo5 = 0;
+	public static int contenidoInicialNodo6 = 2;
+	public static int contenidoInicialNodo7 = 0;
+	public static int contenidoInicialNodo8 = 0;
+	public static int contenidoInicialNodo9 = 0;
+	public static int contenidoInicialNodo10 = 0;
+	public static int contenidoInicialNodo11 = 2;
+	public static int contenidoInicialNodo12 = 0;
+	public static int contenidoInicialNodo13 = 0;
+	public static int contenidoInicialNodo14 = 2;
+	public static int contenidoInicialNodo15 = 0;
+	public static int contenidoInicialNodo16 = 0;
+	public static int contenidoInicialNodo17 = 0;
+	public static int contenidoInicialNodo18 = 0;
+	public static int contenidoInicialNodo19 = 0;
+	public static int contenidoInicialNodo20 = 0;
+	public static int contenidoInicialNodo21 = 0;
+	public static int contenidoInicialNodo22 = 0;
+	public static int contenidoInicialNodo23 = 2;
+	public static int contenidoInicialNodo24 = 3;
+	public static int contenidoInicialNodo25 = 0;
+	public static int contenidoInicialNodo26 = 0;
+	public static int contenidoInicialNodo27 = 0;
+	public static int contenidoInicialNodo28 = 0;
+	public static int contenidoInicialNodo29 = 0;
+	
+	public static ArrayList<Integer> sucesoresNodo1 = new ArrayList<Integer>(List.of(2, 4));
+	public static ArrayList<Integer> sucesoresNodo2 = new ArrayList<Integer>(List.of(1, 3));
+	public static ArrayList<Integer> sucesoresNodo3 = new ArrayList<Integer>(List.of(2, 4));
+	public static ArrayList<Integer> sucesoresNodo4 = new ArrayList<Integer>(List.of(1, 3, 5, 7));
+	public static ArrayList<Integer> sucesoresNodo5 = new ArrayList<Integer>(List.of(6, 9));
+	public static ArrayList<Integer> sucesoresNodo6 = new ArrayList<Integer>(List.of(5, 7));
+	public static ArrayList<Integer> sucesoresNodo7 = new ArrayList<Integer>(List.of(4, 6, 8, 29));
+	public static ArrayList<Integer> sucesoresNodo8 = new ArrayList<Integer>(List.of(7, 9, 11, 28));
+	public static ArrayList<Integer> sucesoresNodo9 = new ArrayList<Integer>(List.of(5, 8, 10));
+	public static ArrayList<Integer> sucesoresNodo10 = new ArrayList<Integer>(List.of(9));
+	public static ArrayList<Integer> sucesoresNodo11 = new ArrayList<Integer>(List.of(8, 12, 28));
+	public static ArrayList<Integer> sucesoresNodo12 = new ArrayList<Integer>(List.of(11, 13));
+	public static ArrayList<Integer> sucesoresNodo13 = new ArrayList<Integer>(List.of(12, 14, 22, 26, 27));
+	public static ArrayList<Integer> sucesoresNodo14 = new ArrayList<Integer>(List.of(13, 15, 16));
+	public static ArrayList<Integer> sucesoresNodo15 = new ArrayList<Integer>(List.of(14));
+	public static ArrayList<Integer> sucesoresNodo16 = new ArrayList<Integer>(List.of(14, 17));
+	public static ArrayList<Integer> sucesoresNodo17 = new ArrayList<Integer>(List.of(16, 18, 22));
+	public static ArrayList<Integer> sucesoresNodo18 = new ArrayList<Integer>(List.of(17, 19));
+	public static ArrayList<Integer> sucesoresNodo19 = new ArrayList<Integer>(List.of(18, 20));
+	public static ArrayList<Integer> sucesoresNodo20 = new ArrayList<Integer>(List.of(19, 21, 24));
+	public static ArrayList<Integer> sucesoresNodo21 = new ArrayList<Integer>(List.of(20, 22));
+	public static ArrayList<Integer> sucesoresNodo22 = new ArrayList<Integer>(List.of(13, 17, 21, 23));
+	public static ArrayList<Integer> sucesoresNodo23 = new ArrayList<Integer>(List.of(22, 24));
+	public static ArrayList<Integer> sucesoresNodo24 = new ArrayList<Integer>(List.of(20, 23, 25));
+	public static ArrayList<Integer> sucesoresNodo25 = new ArrayList<Integer>(List.of(24, 26));
+	public static ArrayList<Integer> sucesoresNodo26 = new ArrayList<Integer>(List.of(13, 25, 27, 29));
+	public static ArrayList<Integer> sucesoresNodo27 = new ArrayList<Integer>(List.of(13, 26, 28));
+	public static ArrayList<Integer> sucesoresNodo28 = new ArrayList<Integer>(List.of(8, 11, 27));
+	public static ArrayList<Integer> sucesoresNodo29 = new ArrayList<Integer>(List.of(7, 26));
+
 }
--- src/frsf/ia/tp1/pokemon/PokemonAgentState.java
+++ src/frsf/ia/tp1/pokemon/PokemonAgentState.java
@@ -3,6 +3,7 @@ package frsf.ia.tp1.pokemon;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import frsf.cidisi.faia.agent.Perception;
 import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
@@ -50,16 +51,105 @@ public class PokemonAgentState extends SearchBasedAgentState {
 		return (this.energia > 0);
 	}
 	
+	public void curar(Integer cantidadVida) {
+		
+		return;
+	}
+	
+	public boolean noTieneQuePelear() {
+		
+		boolean noTieneQuePelear = false;
+		
+		if(this.mapaAgente.get(nodoActual) == 0 || this.escapo) {
+			noTieneQuePelear = true;
+		}
+		
+		return noTieneQuePelear;
+	}
+	
 	private LinkedHashMap<Integer, Integer> cargarMapaAgente() {
-		return null;
+
+		LinkedHashMap<Integer, Integer> mapaAgenteInicial = new LinkedHashMap<>();
+
+		mapaAgenteInicial.put(1, Const.contenidoInicialNodo1);
+		mapaAgenteInicial.put(2, Const.contenidoInicialNodo2);
+		mapaAgenteInicial.put(3, Const.contenidoInicialNodo3);
+		mapaAgenteInicial.put(4, Const.contenidoInicialNodo4);
+		mapaAgenteInicial.put(5, Const.contenidoInicialNodo5);
+		mapaAgenteInicial.put(6, Const.contenidoInicialNodo6);
+		mapaAgenteInicial.put(7, Const.contenidoInicialNodo7);
+		mapaAgenteInicial.put(8, Const.contenidoInicialNodo8);
+		mapaAgenteInicial.put(9, Const.contenidoInicialNodo9);
+		mapaAgenteInicial.put(10, Const.contenidoInicialNodo10);
+		mapaAgenteInicial.put(11, Const.contenidoInicialNodo11);
+		mapaAgenteInicial.put(12, Const.contenidoInicialNodo12);
+		mapaAgenteInicial.put(13, Const.contenidoInicialNodo13);
+		mapaAgenteInicial.put(14, Const.contenidoInicialNodo14);
+		mapaAgenteInicial.put(15, Const.contenidoInicialNodo15);
+		mapaAgenteInicial.put(16, Const.contenidoInicialNodo16);
+		mapaAgenteInicial.put(17, Const.contenidoInicialNodo17);
+		mapaAgenteInicial.put(18, Const.contenidoInicialNodo18);
+		mapaAgenteInicial.put(19, Const.contenidoInicialNodo19);
+		mapaAgenteInicial.put(20, Const.contenidoInicialNodo20);
+		mapaAgenteInicial.put(21, Const.contenidoInicialNodo21);
+		mapaAgenteInicial.put(22, Const.contenidoInicialNodo22);
+		mapaAgenteInicial.put(23, Const.contenidoInicialNodo23);
+		mapaAgenteInicial.put(24, Const.contenidoInicialNodo24);
+		mapaAgenteInicial.put(25, Const.contenidoInicialNodo25);
+		mapaAgenteInicial.put(26, Const.contenidoInicialNodo26);
+		mapaAgenteInicial.put(27, Const.contenidoInicialNodo27);
+		mapaAgenteInicial.put(28, Const.contenidoInicialNodo28);
+		mapaAgenteInicial.put(29, Const.contenidoInicialNodo29);
+		
+		return mapaAgenteInicial;
 	}
 	
 	private HashMap<Integer, ArrayList<Integer>> cargarMapaSucesoresAgente() {
-		return null;
+		
+		HashMap<Integer, ArrayList<Integer>> mapaSucesoresAgenteInicial = new HashMap<>();
+		
+		mapaSucesoresAgenteInicial.put(1, Const.sucesoresNodo1);
+		mapaSucesoresAgenteInicial.put(2, Const.sucesoresNodo2);
+		mapaSucesoresAgenteInicial.put(3, Const.sucesoresNodo3);
+		mapaSucesoresAgenteInicial.put(4, Const.sucesoresNodo4);
+		mapaSucesoresAgenteInicial.put(5, Const.sucesoresNodo5);
+		mapaSucesoresAgenteInicial.put(6, Const.sucesoresNodo6);
+		mapaSucesoresAgenteInicial.put(7, Const.sucesoresNodo7);
+		mapaSucesoresAgenteInicial.put(8, Const.sucesoresNodo8);
+		mapaSucesoresAgenteInicial.put(9, Const.sucesoresNodo9);
+		mapaSucesoresAgenteInicial.put(10, Const.sucesoresNodo10);
+		mapaSucesoresAgenteInicial.put(11, Const.sucesoresNodo11);
+		mapaSucesoresAgenteInicial.put(12, Const.sucesoresNodo12);
+		mapaSucesoresAgenteInicial.put(13, Const.sucesoresNodo13);
+		mapaSucesoresAgenteInicial.put(14, Const.sucesoresNodo14);
+		mapaSucesoresAgenteInicial.put(15, Const.sucesoresNodo15);
+		mapaSucesoresAgenteInicial.put(16, Const.sucesoresNodo16);
+		mapaSucesoresAgenteInicial.put(17, Const.sucesoresNodo17);
+		mapaSucesoresAgenteInicial.put(18, Const.sucesoresNodo18);
+		mapaSucesoresAgenteInicial.put(19, Const.sucesoresNodo19);
+		mapaSucesoresAgenteInicial.put(20, Const.sucesoresNodo20);
+		mapaSucesoresAgenteInicial.put(21, Const.sucesoresNodo21);
+		mapaSucesoresAgenteInicial.put(22, Const.sucesoresNodo22);
+		mapaSucesoresAgenteInicial.put(23, Const.sucesoresNodo23);
+		mapaSucesoresAgenteInicial.put(24, Const.sucesoresNodo24);
+		mapaSucesoresAgenteInicial.put(25, Const.sucesoresNodo25);
+		mapaSucesoresAgenteInicial.put(26, Const.sucesoresNodo26);
+		mapaSucesoresAgenteInicial.put(27, Const.sucesoresNodo27);
+		mapaSucesoresAgenteInicial.put(28, Const.sucesoresNodo28);
+		mapaSucesoresAgenteInicial.put(29, Const.sucesoresNodo29);
+		
+		return mapaSucesoresAgenteInicial;
 	}
 	
 	private ArrayList<Integer> cargarTurnosRestantesParaUtilizarAtaquesEspeciales() {
-		return null;
+		
+		ArrayList<Integer> turnosRestantesParaUtilizarAtaquesEspecialInicial = new ArrayList<>();
+		
+		turnosRestantesParaUtilizarAtaquesEspecialInicial.add(1, -1);
+		turnosRestantesParaUtilizarAtaquesEspecialInicial.add(2, -1);
+		turnosRestantesParaUtilizarAtaquesEspecialInicial.add(3, -1);
+		
+		return turnosRestantesParaUtilizarAtaquesEspecialInicial;
 	}
 
 
